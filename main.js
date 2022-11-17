225fb81acd9f2d7efec929b64ea4b5072d7a6278
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-17 13:22:52 -0300

    add/remove modal

--- main.js
+++ main.js
@@ -3,13 +3,28 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-// 3 Maneiras de criar e atribuir função a um evento
-form.onsubmit = function(event) {
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+
+form.onsubmit = event => {
   event.preventDefault()
 
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  console.log(weight, height)
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
 }
 
+modalBtnClose.onclick = () => {
+  modalWrapper.classList.remove('open')
+}
+
+function IMC (weight, height) {
+  return (weight / ((height / 100) **2)).toFixed(2)
+}
